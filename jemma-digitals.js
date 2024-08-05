048b19e025ddae0dbe69df8ec4b4275b0ea1f41e
Warith Lawal <79901863+Warithlawal@users.noreply.github.com>
2024-08-05 13:49:20 +0100

    Add files via upload

--- jemma-digitals.js
+++ jemma-digitals.js
@@ -57,3 +57,120 @@ faqs.forEach((faq) => {
         faq.classList.toggle('active');
     });
 });
+
+
+/*const contactForm = document.getElementById('applicationForm'),
+      contactMessage = document.getElementById('contact-message'),
+      application = document.getElementById('form_container');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_km7balj', 'template_uz9rorp', '#applicationForm', 'Cshuz3q2Ul1WqyboJ')
+    .then(() => {
+        application.style.display = 'none';  // Hide the form
+        contactMessage.style.display = 'block'; // Show the message
+
+        setTimeout(() => {
+            contactMessage.style.display = 'none';
+            contactMessage.textContent = ''; // Clear the message text
+        }, 5000);
+
+        contactForm.reset();
+    })
+    .catch((error) => {
+        contactMessage.style.display = 'block'; // Show the message
+        contactMessage.innerHTML = '<img src="images/error.png" alt="Error"><span>Failed to send application. Please try again later.</span>';
+
+        setTimeout(() => {
+            contactMessage.style.display = 'none';
+            contactMessage.innerHTML = ''; // Clear the message text
+        }, 5000);
+    });
+};
+
+contactForm.addEventListener('submit', sendEmail);*/
+
+
+const scheduleCall = [{
+    date: '',
+    time: ''
+}];
+
+renderSchedule();
+
+function renderSchedule() {
+
+    let scheduleCallHTML = '';
+
+    for (let i = 0; i < scheduleCall.length; i++) {
+        const scheduleObject = scheduleCall[i];
+        const { date, time} = scheduleObject
+        const html = `<p>
+        ${date} ${time}
+        </p>`;
+        scheduleCallHTML += html;
+    }
+
+    document.querySelector('.js-schedule').innerHTML = scheduleCallHTML;
+}
+
+function addDate() {
+    const inputElement = document.querySelector('.js-date-input');
+    const date = inputElement.value;
+
+    const timeInputElement = document.querySelector('.js-time-input');
+    const time = timeInputElement.value;
+    
+    scheduleCall.push({
+        date,
+        time
+    });
+
+    inputElement.value = '';
+    timeInputElement.value = '';
+
+    renderSchedule();
+}
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay2 = document.getElementById('overlay-2');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+        body.classList.add('no-scroll');
+    });
+});
+
+overlay2.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay2.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay2.classList.remove('active');
+}
+
+
+
